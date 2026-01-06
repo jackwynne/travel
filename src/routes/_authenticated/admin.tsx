4a3d45bb69cdd5289835af722c3a7479d2042d08
jackwynne <jack.wynne@outlook.com>
2026-01-06 16:42:30 +1300

    Somewhat fix header

--- src/routes/_authenticated/admin.tsx
+++ src/routes/_authenticated/admin.tsx
@@ -55,8 +55,8 @@ function AdminLayout() {
 			}
 		>
 			<AdminSidebar />
-			<SidebarInset>
-				<header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+			<SidebarInset className="">
+				<header className="overflow-y-auto overscroll-none sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b bg-background px-4">
 					<SidebarTrigger className="-ml-1" />
 					<Separator
 						orientation="vertical"
